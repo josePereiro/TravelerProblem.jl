438739765afa81b42751ff1769223388e5320b8c
Jose Antonio Pereiro Morejon <jose.pereiro.lab@gmail.com>
2019-04-02 20:11:25 -0400

    Files generated by PkgTemplates

---
+++ src/TravelerProblem.jl
@@ -0,0 +1,5 @@
+module TravelerProblem
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TravelerProblem
+using Test
+
+@testset "TravelerProblem.jl" begin
+    # Write your own tests here.
+end
